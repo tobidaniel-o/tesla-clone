4693c2b8a39324eb85ecd7bb185ba52d0119890c
Oluwatobi Okunuga <tobi@Oluwatobis-MacBook-Air.local>
2022-08-30 10:34:20 -0400

    code cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,14 +28,6 @@ function Header() {
           <CustomClose onClick={() => setBurgerStatus(false)}/>
         </CloseWrapper>
         {cars && cars.map((car, index) => <li key={index}><a href="#">{car}</a></li>)}
-        {/* <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Used Inventory</a></li>
-        <li><a href="#">Trade-in</a></li>
-        <li><a href="#">Cybertruck</a></li>
-        <li><a href="#">Roadster</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li> */}
       </BurgerNav>
     </Container>
   )
